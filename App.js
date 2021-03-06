25cc2759403b718cf7dcfcf799372e1ac788454a
Kaushttav Karan <kaushttavkaran@gmail.com>
2021-03-06 11:32:00 +0530

    message

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { StyleSheet, Text, View, Image } from 'react-native';
+//import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+
+import WriteStoryScreen from './screens/WriteStoryScreen'
+import ReadStoryScreen from './screens/ReadStoryScreen'
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <AppContainer/>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const TabNavigator = createBottomTabNavigator({
+  ReadStory : {screen : ReadStoryScreen},
+  WriteStory : {screen : WriteStoryScreen}
+},
+{
+  defaultNavigationOptions : ({navigation})=>({
+    tabBarIcon : ({})=>{
+      const routeName = navigation.state.routeName
+      if(routeName == 'ReadStory'){
+        return(
+          <Image
+          source = {require('./assets/read.png')}
+          style = {{width : 40 , height : 40}}/>
+        )
+      }
+      else if(routeName == 'WriteStory'){
+        return(
+          <Image
+          source = {require('./assets/write.png')}
+          style = {{width : 40 , height : 40}}/>
+        )
+      }
+    }
+  })
 });
